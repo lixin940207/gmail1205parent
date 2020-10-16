4f30e4fa7dccf5572e77f176515c9dfc55579592
lixin-940207 <alix.xinli@gmail.com>
2020-10-16 21:48:56 +0800

    finished logger module

--- gmall1205-logger/src/main/java/com/atguigu/gmall1205/logger/controller/LoggerController.java
+++ gmall1205-logger/src/main/java/com/atguigu/gmall1205/logger/controller/LoggerController.java
@@ -2,7 +2,10 @@ package com.atguigu.gmall1205.logger.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall1205.commom.constant.GmallConstant;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //= controller+responsebody
 public class LoggerController {
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
 
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(LoggerController.class) ;
     @PostMapping("/log")
@@ -21,6 +27,13 @@ public class LoggerController {
         jsonObject.put("ts",System.currentTimeMillis());
         // 落盘到logfile   log4j
         logger.info(jsonObject.toJSONString());
+
+        //发送kafka
+        if("startup".equals(jsonObject.getString("type")) ){
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_STARTUP,jsonObject.toJSONString());
+        }else{
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_EVENT,jsonObject.toJSONString());
+        }
         return "success";
     }
 }
