20d9bd7faa454398bec0cf5d466b7f1da8dfec23
lixin-940207 <alix.xinli@gmail.com>
2020-10-28 09:48:05 +0800

    abandon jedisPool, it cause some thread problems when data volume become large

--- gmall1205-realtime/src/main/scala/com/atguigu/gmall1205/realtime/util/RedisUtil.scala
+++ gmall1205-realtime/src/main/scala/com/atguigu/gmall1205/realtime/util/RedisUtil.scala
@@ -1,18 +1,22 @@
 package com.atguigu.gmall1205.realtime.util
 
-import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
+import redis.clients.jedis.Jedis
+
 
 object RedisUtil {
 
-  var jedisPool:JedisPool=null
+  //var jedisPool:JedisPool=null
 
+    val config = PropertiesUtil.load("config.properties")
+    val host = config.getProperty("redis.host")
+    val port = config.getProperty("redis.port")
   def getJedisClient: Jedis = {
-    if(jedisPool==null){
-      //      println("开辟一个连接池")
-      val config = PropertiesUtil.load("config.properties")
-      val host = config.getProperty("redis.host")
-      val port = config.getProperty("redis.port")
-
+//    if(jedisPool==null){
+//      //      println("开辟一个连接池")
+//      val config = PropertiesUtil.load("config.properties")
+//      val host = config.getProperty("redis.host")
+//      val port = config.getProperty("redis.port")
+      /*
       val jedisPoolConfig = new JedisPoolConfig()
       jedisPoolConfig.setMaxTotal(100)  //最大连接数
       jedisPoolConfig.setMaxIdle(40)   //最大空闲
@@ -22,11 +26,18 @@ object RedisUtil {
       jedisPoolConfig.setTestOnBorrow(true) //每次获得连接的进行测试
       jedisPoolConfig.setTestOnReturn(true)
 
-      jedisPool=new JedisPool(jedisPoolConfig,host,port.toInt)
-    }
+      jedisPool=new JedisPool(jedisPoolConfig,host,port.toInt, 1000*60)
+
+       */
+    // }
     //    println(s"jedisPool.getNumActive = ${jedisPool.getNumActive}")
     //   println("获得一个连接")
-    jedisPool.getResource
+    //jedisPool.getResource
+
+
+      val jedis: Jedis = new Jedis(host, port.toInt, 1000 * 60)
+      jedis.connect()
+      jedis
   }
 }
 
