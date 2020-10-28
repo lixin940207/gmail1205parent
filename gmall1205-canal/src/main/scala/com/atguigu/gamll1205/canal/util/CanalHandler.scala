036de4570a2f3713c9cc2f77a48eab37606d4383
lixin-940207 <alix.xinli@gmail.com>
2020-10-28 09:24:48 +0800

    restructure canal write to Kafka logic

--- gmall1205-canal/src/main/scala/com/atguigu/gamll1205/canal/util/CanalHandler.scala
+++ gmall1205-canal/src/main/scala/com/atguigu/gamll1205/canal/util/CanalHandler.scala
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONObject
 import com.alibaba.otter.canal.protocol.CanalEntry
 import com.alibaba.otter.canal.protocol.CanalEntry.{EventType, RowData}
 import com.atguigu.gmall1205.common.constant.GmallConstant
+import scala.collection.JavaConversions._
 
 
 
@@ -20,20 +21,26 @@ object CanalHandler {
   def handle(tableName: String, eventType: EventType, rowDataList: util.List[RowData]) = {
     import scala.collection.JavaConversions._
     if ("order_info" == tableName && eventType == EventType.INSERT && rowDataList.size() > 0) {
-      // 1. rowData 表示一行数据, 通过他得到每一列. 首先遍历每一行数据
-      for (rowData <- rowDataList) {
-        val jsonObj = new JSONObject()
-        // 2. 得到每行中, 所有列组成的列表
-        val columnList: util.List[CanalEntry.Column] = rowData.getAfterColumnsList
-        for (column <- columnList) {
-          // 3. 得到列名和列值
-          val key = column.getName
-          val value = column.getValue
-          jsonObj.put(key, value)
-        }
-        MyKafkaUtil.send(GmallConstant.ORDER_TOPIC, jsonObj.toString)
+      sendRowListToKafka(rowDataList, GmallConstant.ORDER_TOPIC)
+    } else if ("order_detail" == tableName && eventType == EventType.INSERT && rowDataList != null && !rowDataList.isEmpty) {
+      sendRowListToKafka(rowDataList, GmallConstant.DETAIL_TOPIC)
+    }
+
+  }
+  private def sendRowListToKafka(rowDataList: util.List[CanalEntry.RowData], topic: String): Unit = {
+    for (rowData <- rowDataList) {
+
+      val jsonObj = new JSONObject()
+      // 变化后的列
+      val columnList: util.List[CanalEntry.Column] = rowData.getAfterColumnsList
+      for (column <- columnList) {
+        jsonObj.put(column.getName, column.getValue)
       }
+      println(jsonObj.toJSONString)
+      // 写入到Kafka
+      MyKafkaUtil.send(topic, jsonObj.toJSONString)
     }
   }
+
 }
 
