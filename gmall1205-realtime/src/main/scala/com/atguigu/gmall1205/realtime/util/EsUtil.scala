e2d6c96b48b654c004fd77187cc81fe4307265e5
lixin-940207 <alix.xinli@gmail.com>
2020-10-28 09:27:05 +0800

    add EsUtil

---
+++ gmall1205-realtime/src/main/scala/com/atguigu/gmall1205/realtime/util/EsUtil.scala
@@ -0,0 +1,82 @@
+package com.atguigu.gmall1205.realtime.util
+
+import com.atguigu.gmall1205.realtime.bean.AlertInfo
+import io.searchbox.client.{JestClient, JestClientFactory}
+import io.searchbox.client.config.HttpClientConfig
+import io.searchbox.core.{Bulk, Index}
+import org.apache.spark.rdd.RDD
+
+object EsUtil {
+    val esUrl = "http://hadoop102:9200"
+    val factory = new JestClientFactory
+    val conf: HttpClientConfig = new HttpClientConfig.Builder(esUrl)
+            .multiThreaded(true)
+            .maxTotalConnection(20)
+            .connTimeout(10000)
+            .readTimeout(10000)
+            .build()
+    factory.setHttpClientConfig(conf)
+
+    // 获取客户端
+    def getESClient = factory.getObject
+
+    // 插入单条数据
+    def insertSingle(indexName: String, source: Any, id:String=null) = {
+        val client: JestClient = getESClient
+        val index: Index = new Index.Builder(source)
+                .`type`("_doc")
+                .index(indexName)
+                .id(id)
+                .build()
+        client.execute(index)
+        client.close()
+    }
+
+    // 插入多条数据 sources:   Iterable[(id, caseClass)] 或者 Iterable[caseClass]
+    def insertBulk(indexName: String, sources: Iterable[Any]): Unit = {
+        if (sources.isEmpty) return
+
+        val client: JestClient = getESClient
+        val bulkBuilder = new Bulk.Builder()
+                .defaultIndex(indexName)
+                .defaultType("_doc")
+        sources.foreach { // 把所有的source变成action添加buck中
+            //传入的是值是元组, 第一个表示id
+            case (id: String, data) => bulkBuilder.addAction(new Index.Builder(data).id(id).build())
+            // 其他类型 没有id, 将来省的数据会自动生成默认id
+            case data => bulkBuilder.addAction(new Index.Builder(data).build())
+        }
+        client.execute(bulkBuilder.build())
+        closeClient(client)
+    }
+
+    /**
+     * 关闭客户端
+     *
+     * @param client
+     */
+    def closeClient(client: JestClient) = {
+        if (client != null) {
+            try {
+                client.shutdownClient()
+            } catch {
+                case e => e.printStackTrace()
+            }
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+        val source =
+            """
+              |{
+              | "age":10,
+              | "name": "lisi"
+              |}
+              |""".stripMargin
+        insertSingle("user", source, "100")
+        insertBulk("user", Iterable(User( 20, "aa"), User(30, "bb")))
+    }
+
+}
+
+case class User(age:Int, name:String)
